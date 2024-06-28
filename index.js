b2c32efb3ef597c6d4c91196a65f1747a1e1bb7b
ying1488 <yingecteach@gmail.com>
2024-06-28 17:20:02 +1000

    deletion Mongoose Middleware delete farm and associated data

--- index.js
+++ index.js
@@ -34,12 +34,16 @@ app.get('/farms/new', (req, res) => {
   res.render('farms/new')
 })
 
-app.get('/farms/:id', async (req, res) => {
-  const farm = await Farm.findById(req.params.id).populate('products')
-  console.log(farm)
-  res.render('farms/show', { farm })
+app.delete('/farms/:id', async (req, res) => {
+  const farm = await Farm.findByIdAndDelete(req.params.id);
+  res.redirect('/farms');
 })
 
+app.get('/farms/:id', async (req, res) => {
+  const farm = await Farm.findById(req.params.id).populate('products')
+
+  res.render('farms/show', { farm })
+})
 
 app.post('/farms', async (req, res) => {
   const farm = new Farm(req.body);
